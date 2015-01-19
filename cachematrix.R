8e4415bb0dc5d0aa3841a88564cfef0d1d5d810a
Jos van Loon <jjjvanloon@gmail.com>
2015-01-19 20:41:20 +0100

    end solution assignment 2
    my solution for assignment 2. This assignment is about caching the
    inverse of a matrix.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Hi Coursera!
+## These functions save time by caching the inverse of a matrix
+## preventing calculating the same inverse multiple times
 
-## Write a short comment describing this function
+## This function makes a cached version of a matrix. 
+## This cache is capable to also save the inverse of the matrix, 
+## when calculated with cacheSolve 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinv <- function(solve) m <<- solve
+        getinv <- function() m
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## This function solves the inverse of a matrix and stores that in the cache made by makeCacheMatrix.
+## This function is also capable of comparing a control matrix, given in a seperate argument with the cached matrix.
+## And notifies the user if the control matrix is different from the cached matrix.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, c = "empty",...) {
+        m <- x$getinv()
+        if (identical(c,"empty") & !is.null(m)) {
+                message("getting cached inverse matrix")
+                return(m)
+        } else if(identical(x$get(),c) & !is.null(m)) {
+                message("matrix unchanged")
+                message("getting cached inverse matrix")
+                return(m)
+        } else if (!identical(x$get(),c)& !identical(c,"empty")) {
+                message("The given control matrix is not identical to the cached matrix.")
+                message("Make a new cache with makeCacheMatrix.")
+        } else {
+                data <- x$get()
+                m <- solve(data, ...)
+                x$setinv(m)
+                m
+        }
 }
